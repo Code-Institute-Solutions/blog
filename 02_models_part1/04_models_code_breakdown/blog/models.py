86069680962e773bc8eb66020b705fce2529f7f9
NielMc <niel@codeinstitute.net>
2023-09-07 12:29:48 +0100

    adds the models part one code

---
+++ 02_models_part1/04_models_code_breakdown/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
