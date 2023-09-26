04619bacceab30046e260d8aa9c1fa92afbc25ae
NielMc <niel@codeinstitute.net>
2023-09-26 09:35:40 +0100

    match code to lesson content

--- 02_models_part1/03_build_post_model/codestar/settings.py
+++ 02_models_part1/03_build_post_model/codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-ajgreaves-blog-lesson-pl-lfv6kgfdws.us2.codeanyapp.com",
