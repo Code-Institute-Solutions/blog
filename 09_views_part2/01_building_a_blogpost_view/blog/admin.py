16a23a308a9e4e3875251967d8f57595ca40c98e
NielMc <niel@codeinstitute.net>
2023-10-13 16:37:03 +0100

    views 2 part 1

---
+++ 09_views_part2/01_building_a_blogpost_view/blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+# Register your models here.
+admin.site.register(Comment)
---
+++ 09_views_part2/01_building_a_blogpost_view/blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Post
+
+# Create your views here.
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+
+def post_detail(request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post},)
