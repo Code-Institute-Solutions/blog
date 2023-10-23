4dc202449795d257b7a0fbcffe9fad1ac537cad1
NielMc <niel@codeinstitute.net>
2023-10-23 12:53:43 +0100

    standardises installed_app order

--- 09_views_part2/01_building_a_blogpost_view/codestar/settings.py
+++ 09_views_part2/01_building_a_blogpost_view/codestar/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'django_summernote',
+    'blog',
 ]
 
 MIDDLEWARE = [
