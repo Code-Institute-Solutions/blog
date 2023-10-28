32b8aa9670e297a4804b75e734f9a425a97a756e
NielMc <niel@codeinstitute.net>
2023-10-28 17:06:44 +0100

    posting to db solution code

---
+++ 12_views_part_3/01_posting_to_database/about/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import About
+# Create your views here.
+
+
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
