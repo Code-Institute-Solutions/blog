190f41783f42839a35fd796446e7d09bc4af1e1c
lechien73 <matt.rudge@codeinstitute.net>
2023-11-01 16:59:05 +0000

    Update collaboration form challenge

--- 13_collaboration_form/01_create_the_form/about/views.py
+++ 13_collaboration_form/01_create_the_form/about/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import About
-# Create your views here.
+from .forms import CollaborateForm
 
 
 def about_me(request):
@@ -8,9 +8,13 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
