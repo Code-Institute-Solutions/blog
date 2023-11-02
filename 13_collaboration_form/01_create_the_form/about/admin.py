2afd5303c7ccc41c76d4825c3b0c746785c8810c
Matt Rudge <matt.rudge@codeinstitute.net>
2023-11-02 15:51:21 +0000

    Update admin.py

--- 13_collaboration_form/01_create_the_form/about/admin.py
+++ 13_collaboration_form/01_create_the_form/about/admin.py
@@ -7,6 +7,13 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
+# Note: admin.ModelAdmin is the standard way of registering
+#       our model with the admin panel. We do it differently
+#       above because we are supplying Summernote fields.
+#       If you want to customise the admin panel view in your
+#       own projects, then inherit from admin.ModelAdmin like
+#       we do here.
+
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
 
