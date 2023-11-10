01394fe67272e91e606af0d17ca8c2773ada619b
NielMc <niel@codeinstitute.net>
2023-11-10 14:35:03 +0000

    added docstring to class-based view

--- 14_where_to_put_things/03_tidy_up/blog/views.py
+++ 14_where_to_put_things/03_tidy_up/blog/views.py
@@ -9,6 +9,20 @@ from .forms import CommentForm
 
 
 class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts. 
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+        
+    **Template:**
+
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
