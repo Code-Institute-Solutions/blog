ed1a76cc0e1eba15680ed038a6a07e8deac84c4c
NielMc <niel@codeinstitute.net>
2023-11-10 08:54:26 +0000

    update about_me view

--- 14_where_to_put_things/01_storing_images_in_cloudinary/about/views.py
+++ 14_where_to_put_things/01_storing_images_in_cloudinary/about/views.py
@@ -9,22 +9,16 @@ def about_me(request):
     """
     Renders the About page
     """
-    about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            collaborate_request = collaborate_form.save(commit=False)
-            collaborate_request.read = False
-            collaborate_request.save()
+            collaborate_form.save()
             messages.add_message(
                 request, messages.SUCCESS,
-                'Collaboration request received! I endeavor to respond within 2 working days.'
+                'Collaboration request received! I endeavour to respond within 2 working days.'
             )
-            collaborate_form = CollaborateForm()
-        else:
-            collaborate_form = CollaborateForm()
-    else:
-        collaborate_form = CollaborateForm()
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
